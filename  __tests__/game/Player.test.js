e30e1cb97b37055555102365c0496a194647fc81
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2019-09-19 20:48:13 -0300

    update na posicao original do player

---  __tests__/game/Player.test.js
+++  __tests__/game/Player.test.js
@@ -4,57 +4,57 @@ const player = new Player(config);
 
 it('text move player to up', () => {
     player.moveUp();
-    expect(player.positionX).toBe(400);
-    expect(player.positionY).toBe(395);
+    expect(player.positionX).toBe(375);
+    expect(player.positionY).toBe(370);
 });
 
 it('text move player to left', () => {
     player.moveLeft();
-    expect(player.positionX).toBe(395);
-    expect(player.positionY).toBe(395);
+    expect(player.positionX).toBe(370);
+    expect(player.positionY).toBe(370);
 });
 
 it('text move player to left + up', () => {
     player.moveLeftUp();
-    expect(player.positionX).toBe(390);
-    expect(player.positionY).toBe(390);
+    expect(player.positionX).toBe(365);
+    expect(player.positionY).toBe(365);
 });
 
 it('text move player to left + down', () => {
     player.moveLeftDown();
-    expect(player.positionX).toBe(385);
-    expect(player.positionY).toBe(395);
+    expect(player.positionX).toBe(360);
+    expect(player.positionY).toBe(370);
 });
 
 it('text move player to right', () => {
     player.moveRight();
-    expect(player.positionX).toBe(390);
-    expect(player.positionY).toBe(395);
+    expect(player.positionX).toBe(365);
+    expect(player.positionY).toBe(370);
 });
 
 it('text move player to right + up', () => {
     player.moveRightUp();
-    expect(player.positionX).toBe(395);
-    expect(player.positionY).toBe(390);
+    expect(player.positionX).toBe(370);
+    expect(player.positionY).toBe(365);
 });
 
 it('text move player to right + down', () => {
     player.moveRightDown();
-    expect(player.positionX).toBe(400);
-    expect(player.positionY).toBe(395);
+    expect(player.positionX).toBe(375);
+    expect(player.positionY).toBe(370);
 });
 
 it('text move player to down', () => {
     player.moveDown();
-    expect(player.positionX).toBe(400);
-    expect(player.positionY).toBe(400);
+    expect(player.positionX).toBe(375);
+    expect(player.positionY).toBe(375);
 });
 
 it('text forced move player to up', () => {
     player.positionY = 5;
     player.moveUp();
     player.moveUp();
-    expect(player.positionX).toBe(400);
+    expect(player.positionX).toBe(375);
     expect(player.positionY).toBe(0);
 });
 
@@ -62,7 +62,7 @@ it('text forced move player to down', () => {
     player.positionY = 545;
     player.moveDown();
     player.moveDown();
-    expect(player.positionX).toBe(400);
+    expect(player.positionX).toBe(375);
     expect(player.positionY).toBe(550);
 });
 
